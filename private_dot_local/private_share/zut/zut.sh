21b96c7582149a3fdeefc7529c976b7e09ccfceb
Yannick Jeandroz <jyannick.github@gmail.com>
2022-06-23 19:00:38 +0200

    fix 'column' issue on suse

--- private_dot_local/private_share/zut/zut.sh
+++ private_dot_local/private_share/zut/zut.sh
@@ -1,16 +1,18 @@
 #! /bin/zsh
 
 zut_dir=${0:a:h}
-separator='µ' # single character, must not appear in the txt files
+intermediary_separator='µ' # single character, must not appear in the txt files
+display_separator='   |   ' # can be multiple characters, must look fine on screen
+display_splitter='|' # single character contained in display_separator
 
 _zut() {
     grep '' -H ${zut_dir}/*.txt \
 	| sed "s|${zut_dir}/||g" \
-	| sed s"|.txt:|${separator}|g" \
-	| sed "s| => |${separator}|g" \
-    | column --table --separator ${separator} --table-columns-limit 3 \
+	| sed s"|.txt:|${intermediary_separator}|g" \
+	| sed "s| => |${intermediary_separator}|g" \
+	| column --table --separator ${intermediary_separator} --output-separator ${display_separator} \
 	| fzf --height 40% \
-	| awk -F"${separator}" '{print $2}'
+	| awk -F"${display_splitter}" '{gsub(/[ \t]+$/, "", $2); gsub(/^[ \t]+/, "", $2); print $2}'
 }
 
 zut() {
